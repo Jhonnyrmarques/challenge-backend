fe8e82f6e668fda29a08b59fc30aae34609df360
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-05 12:22:33 -0300

    feat: create InMemoryUserRegistrationRepository

---
+++ src/repositories/in-memory/in-memory-user-registration-repository.ts
@@ -0,0 +1,37 @@
+import { Prisma, User } from '@prisma/client'
+import { randomUUID } from 'crypto'
+
+import { UserRegistrationRepository } from '../user-registration-repository'
+
+
+export class InMemoryUserRegistrationRepository implements UserRegistrationRepository {
+	public users: User[] = []
+
+	async create(data: Prisma.UserCreateInput) {
+		const user = {
+			id: randomUUID(),
+			first_name: data.first_name,
+			last_name: data.last_name,
+			document: data.document,
+			email: data.email,
+			phone_number: data.phone_number,
+			birth_date: new Date(),
+			created_at: new Date(),
+			updated_at: null
+		}
+
+		this.users.push(user)
+
+		return user
+	}
+
+	async findByEmail(email: string) {
+		const user = this.users.find(user => user.email === email)
+
+		if(!user) {
+			return null
+		}
+
+		return user
+	}
+}
