5275888fcb28e84994b3451f5de2946af3676108
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-05 14:35:14 -0300

    feat: implemented document hash

--- src/usecases/user-registration.ts
+++ src/usecases/user-registration.ts
@@ -1,5 +1,10 @@
+import { hash } from 'bcryptjs'
+import dayjs from 'dayjs'
+
 import { UserRegistrationRepository } from '../repositories/user-registration-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@prisma/client'
+
 
 interface IRequest {
 	first_name: string
@@ -10,11 +15,17 @@ interface IRequest {
 	birth_date: Date
 }
 
+interface IResponse {
+	user: User
+}
+
 export class UserRegistrationUseCase {
 	constructor(private userRegistrationRepository: UserRegistrationRepository) {}
 
-	async execute({first_name, last_name, document, email, phone_number, birth_date}: IRequest) {
+	async execute({first_name, last_name, document, email, phone_number, birth_date}: IRequest): Promise<IResponse> {
 		const emailAlreadyExists = await this.userRegistrationRepository.findByEmail(email)
+
+		const document_hash = await hash(document, 6)
 
 		if(emailAlreadyExists) {
 			throw new UserAlreadyExistsError()
@@ -23,10 +34,10 @@ export class UserRegistrationUseCase {
 		const user = await this.userRegistrationRepository.create({
 			first_name,
 			last_name,
-			document,
+			document: document_hash,
 			email,
 			phone_number,
-			birth_date
+			birth_date: dayjs(birth_date).toDate()
 		})
 
 		return {
