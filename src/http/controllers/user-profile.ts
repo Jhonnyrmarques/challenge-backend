12e04f79d2b96f564049c158a2280dc92e5230ad
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-05 17:07:49 -0300

    feat: create user profile controller

---
+++ src/http/controllers/user-profile.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { UserProfileUseCase } from '../../usecases/user-profile'
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { NotFoundError } from '../../usecases/errors/not-found-error'
+
+
+export async function userProfile(request: FastifyRequest, reply: FastifyReply) {
+	const userProfileSchema = z.object({
+		id: z.string()
+	})
+
+	const { id } = userProfileSchema.parse(request.params)
+
+	const usersRepository = new PrismaUsersRepository()
+	const userProfileUseCase = new UserProfileUseCase(usersRepository)
+
+	try{
+		const { user } = await userProfileUseCase.execute({userId: id})
+
+		return reply.status(200).send(user)
+
+	}catch(err) {
+		if(err instanceof NotFoundError) {
+			return reply.status(404).send({message: err.message})
+		}
+
+		throw err
+	}
+
+	
+}
