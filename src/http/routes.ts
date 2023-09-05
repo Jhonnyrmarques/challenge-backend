139aa2bc9a74a27160dd8ecd0bf691aeb0c5a9c7
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-05 19:57:11 -0300

    feat: create update user route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
 import { userRegistration } from './controllers/user-registration'
 import { userProfile } from './controllers/user-profile'
+import { userUpdate } from './controllers/user-update'
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', userRegistration)
 	app.get('/users/:id', userProfile)
+	app.put('/users/:id', userUpdate)
 }   
