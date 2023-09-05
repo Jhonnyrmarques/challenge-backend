3fc9e1438f488cc2d3c7fb31e2beb2d29d830d17
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-05 19:58:02 -0300

    feat: create updateUser method

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -4,4 +4,5 @@ export interface UsersRepository {
  create(data: Prisma.UserCreateInput): Promise<User>
  findByEmail(email: string): Promise<User | null>
  findUserById(id: string): Promise<User | null>
+ updateUser(data: Prisma.UserUpdateInput): Promise<void>
 }
