488aeecf6e98ad49e709f29be2f869a6a183dcef
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 16:04:32 -0300

    feat: create orderDelete controller

---
+++ src/http/controllers/order-delete.ts
@@ -0,0 +1,36 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { PrismaOrdersRepository } from '../../repositories/prisma/prisma-orders-repository'
+import { OrderDeleteUseCase } from '../../usecases/order-delete'
+
+import { NotFoundError } from '../../usecases/errors/not-found-error'
+
+
+export async function orderDelete(request: FastifyRequest, reply: FastifyReply) {
+	
+	const orderDeleteParamsSchema = z.object({
+		id: z.string()
+	})
+
+
+	const { id } = orderDeleteParamsSchema.parse(request.params)
+
+	
+	const prismaOrdersRepository = new PrismaOrdersRepository()
+	const orderDeleteUseCase = new OrderDeleteUseCase(prismaOrdersRepository)
+
+	try{
+		await orderDeleteUseCase.execute({
+			orderId: id,
+		})
+	}catch(err) {
+		if(err instanceof NotFoundError) {
+			return reply.status(404).send({message: err.message})
+		}
+
+		throw err
+	}
+
+	return reply.status(200).send()
+}
