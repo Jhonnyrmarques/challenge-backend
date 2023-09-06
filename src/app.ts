646178faeb077f1eda63bd52bdccc4d13d5e674e
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 19:48:48 -0300

    chore: add users routes and orders routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
-import { appRoutes } from './http/routes'
+
+import { usersRoutes } from './http/controllers/users/routes'
+import { ordersRoutes } from './http/controllers/orders/routes'
+
 import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
+
 
 export const app = fastify()
 
@@ -10,7 +14,8 @@ app.register(fastifyJwt, {
 	secret: env.JWT_SECRET
 })
 
-app.register(appRoutes)
+app.register(usersRoutes)
+app.register(ordersRoutes)
 
 app.setErrorHandler((error, _, reply) => {
 	if(error instanceof ZodError) {
