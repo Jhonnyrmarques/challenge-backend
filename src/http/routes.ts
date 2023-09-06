407f279ebbc86efa1faefb08e0f80c2866608600
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 14:17:15 -0300

    feat: create route for order registration

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,12 +1,17 @@
 import { FastifyInstance } from 'fastify'
+
 import { userRegistration } from './controllers/user-registration'
 import { userProfile } from './controllers/user-profile'
 import { userUpdate } from './controllers/user-update'
 import { userDelete } from './controllers/user-delete'
+
+import { orderRegistration } from './controllers/order-registration'
 
 export async function appRoutes(app: FastifyInstance) {
 	app.post('/users', userRegistration)
 	app.get('/users/:id', userProfile)
 	app.put('/users/:id', userUpdate)
 	app.delete('/users/:id', userDelete)
+
+	app.post('/orders', orderRegistration)
 }   
