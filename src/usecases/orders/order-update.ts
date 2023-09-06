3baf28555b924c2bb08693ccc162fd0a2635d1d0
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 16:41:00 -0300

    refactor: change path of order repository

---
+++ src/usecases/orders/order-update.ts
@@ -0,0 +1,29 @@
+import { OrdersRepository } from '../../repositories/orders-repository'
+import { NotFoundError } from '../errors/not-found-error'
+
+
+interface IRequest {
+  id: string,
+  description?: string
+  price?: number
+  quantity?: number
+}
+
+export class OrderUpdateUseCase {
+	constructor(private ordersRepository: OrdersRepository) {}
+
+	async execute({id, description, price, quantity}: IRequest) {
+		const orderExists = await this.ordersRepository.findById(id)
+
+		if(!orderExists) {
+			throw new NotFoundError('Order')
+		}
+    
+		await this.ordersRepository.updateOrder({
+			id,
+			description,
+			price,
+			quantity
+		})
+	}
+}
