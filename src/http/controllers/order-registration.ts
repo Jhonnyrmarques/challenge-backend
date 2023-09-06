d94b75170155a5b0b74902ddb3dd026b6baabf42
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 15:13:20 -0300

    refactor: remove try catch

--- src/http/controllers/order-registration.ts
+++ src/http/controllers/order-registration.ts
@@ -1,9 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
-
-import { UserAlreadyExistsError } from '../../usecases/errors/user-already-exists-error'
-
 import { PrismaOrdersRepository } from '../../repositories/prisma/prisma-orders-repository'
 import { OrdersRegistrationUseCase } from '../../usecases/order-registration'
 
@@ -27,20 +24,14 @@ export async function orderRegistration(request: FastifyRequest, reply: FastifyR
 	const prismaOrdersRepository = new PrismaOrdersRepository()
 	const orderRegistrationUseCase = new OrdersRegistrationUseCase(prismaOrdersRepository)
 
-	try{
-		await orderRegistrationUseCase.execute({
-			description,
-			quantity,
-			price,
-			user_id
-		})
-	}catch(err) {
-		if(err instanceof UserAlreadyExistsError) {
-			return reply.status(409).send({message: err.message})
-		}
 
-		throw err
-	}
+	await orderRegistrationUseCase.execute({
+		description,
+		quantity,
+		price,
+		user_id
+	})
+	
 
 	return reply.status(201).send()
 }
