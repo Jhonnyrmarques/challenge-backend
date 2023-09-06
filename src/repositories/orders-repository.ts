80211529e7fec985bad8d142126e6ddb51e07523
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 15:21:04 -0300

    feat: create findById method in orders repository

--- src/repositories/orders-repository.ts
+++ src/repositories/orders-repository.ts
@@ -3,4 +3,5 @@ import { Order, Prisma } from '@prisma/client'
 export interface OrdersRepository {
   create(data: Prisma.OrderUncheckedCreateInput): Promise<Order>
   updateOrder(data: Prisma.OrderUncheckedUpdateInput): Promise<void>
+  findById(id: string): Promise<Order | null>
 }
---
+++ src/repositories/prisma/prisma-orders-repository.ts
@@ -0,0 +1,38 @@
+import { Prisma } from '@prisma/client'
+
+import { OrdersRepository } from '../orders-repository'
+import { prisma } from '../../lib/prisma'
+
+export class PrismaOrdersRepository implements OrdersRepository {
+	async create(data: Prisma.OrderUncheckedCreateInput) {
+		const order = await prisma.order.create({
+			data
+		})
+
+		return order
+	}
+
+	async updateOrder(data: Prisma.OrderUncheckedUpdateInput) {
+		await prisma.order.update({
+			where: {
+				id: data.id?.toString()
+			},
+			data: {
+				description: data.description,
+				quantity: data.quantity,
+				price: data.price
+			}
+		})
+	}
+
+	async findById(id: string) {
+		const order = await prisma.order.findUnique({
+			where: {
+				id
+			}
+		})
+
+		return order
+	}
+  
+}
