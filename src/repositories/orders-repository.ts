6216c2c0a4ba2a1dff013ac245e2f5fa27ebcdb6
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 14:18:24 -0300

    feat: create update method

--- src/repositories/orders-repository.ts
+++ src/repositories/orders-repository.ts
@@ -2,4 +2,5 @@ import { Order, Prisma } from '@prisma/client'
 
 export interface OrdersRepository {
   create(data: Prisma.OrderUncheckedCreateInput): Promise<Order>
+  updateOrder(data: Prisma.OrderUncheckedUpdateInput): Promise<void>
 }
