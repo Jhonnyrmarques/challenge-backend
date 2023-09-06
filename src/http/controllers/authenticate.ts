e27657b9c6aee066a513324020298ea02f52b933
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 18:49:44 -0300

    feat: create authenticate controller

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,45 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { AuthenticateUseCase } from '../../usecases/authenticate'
+import { PrismaUsersRepository } from '../../repositories/prisma/prisma-users-repository'
+import { InvalidCredentialError } from '../../usecases/errors/invalid-credential-error'
+
+export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
+
+	const authenticateBodySchema = z.object({
+		email: z.string().email(),
+		document: z.string()
+	})
+
+	const {email, document} = authenticateBodySchema.parse(request.body)
+
+	try {
+		const usersRepository = new PrismaUsersRepository()
+		const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+
+		const { user } =	await authenticateUseCase.execute({
+			email,
+			document
+		})
+
+		const token = await reply.jwtSign({}, {
+			sign: {
+				sub: user.id
+			}
+		})
+
+		return reply.status(200).send({
+			token
+		})
+    
+	}catch(err) {
+		if(err instanceof InvalidCredentialError) {
+			return reply.status(400).send({message: err.message})
+		}
+
+		throw err
+	}
+
+
+}
