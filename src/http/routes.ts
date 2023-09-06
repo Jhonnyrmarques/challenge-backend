ad002b752dce888e73bc3675825fd0e7c02ab852
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 15:08:22 -0300

    feat: create route to list all users

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,16 +2,21 @@ import { FastifyInstance } from 'fastify'
 
 import { userRegistration } from './controllers/user-registration'
 import { userProfile } from './controllers/user-profile'
+import { listUsers } from './controllers/list-users'
 import { userUpdate } from './controllers/user-update'
 import { userDelete } from './controllers/user-delete'
 
 import { orderRegistration } from './controllers/order-registration'
+import { orderUpdate } from './controllers/order-update'
+
 
 export async function appRoutes(app: FastifyInstance) {
+	app.get('/users', listUsers)
 	app.post('/users', userRegistration)
 	app.get('/users/:id', userProfile)
 	app.put('/users/:id', userUpdate)
 	app.delete('/users/:id', userDelete)
 
 	app.post('/orders', orderRegistration)
+	app.put('/orders/:id', orderUpdate)
 }   
