87d4f1650d0714975098e4d526cf9c631d09f64c
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 18:54:01 -0300

    feat: create AuthenticateUseCase

---
+++ src/usecases/authenticate.ts
@@ -0,0 +1,30 @@
+import { compare } from 'bcryptjs'
+import { UsersRepository } from '../repositories/users-repository'
+import { InvalidCredentialError } from './errors/invalid-credential-error'
+
+interface IRequest {
+  email: string
+  document: string
+} 
+
+export class AuthenticateUseCase {
+	constructor(private usersRepository: UsersRepository) {}
+
+	async execute({email, document}: IRequest) {
+		const user = await this.usersRepository.findByEmail(email)
+
+		if(!user) {
+			throw new InvalidCredentialError()
+		}
+
+		const doesDocumentMatches = await compare(document, user.document)
+
+		if(!doesDocumentMatches) {
+			throw new InvalidCredentialError()
+		}
+
+		return {
+			user
+		}
+	}
+}
