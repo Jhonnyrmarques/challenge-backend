f5e8ee7222acddc7cdcbe38ee10df877004eab36
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-06 23:24:30 -0300

    feat: create swaggerConfig

---
+++ src/config/swagger-config.ts
@@ -0,0 +1,17 @@
+export const swaggerConfig = {
+	swagger: {
+		info: {
+			title: 'API REST FullStack Challenge TechSocial',
+			description: 'CRUDs using Swagger, Fastify and Prisma',
+			version: '1.0.0'
+		},
+		externalDocs: {
+			url: 'https://swagger.io',
+			description: 'Find more info here'
+		},
+		schemes: ['http'],
+		consumes: ['application/json'],
+		produces: ['application/json'],
+		
+	}
+}
