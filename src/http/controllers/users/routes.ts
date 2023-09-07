4334db6fd2d492ba2743f32aa9bd14691e02487e
Jhonnyrmarques <jhonnymarkes89@gmail.com>
2023-09-07 15:51:19 -0300

    chore: add users schema

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -9,17 +9,27 @@ import { userDelete } from './user-delete'
 import { authenticate } from './authenticate'
 import { verifyJWT } from '../../middlewares/verify-jwt'
 import { profile } from './profile'
+import { 
+	createUserSchema, 
+	deleteUsersSchema, 
+	getUsersSchema, 
+	profileSchema, 
+	sessionSchema, 
+	showUsersSchema,
+	updateUserSchema
+} 
+	from '../../schemas/users-schema'
+
 
 export async function usersRoutes(app: FastifyInstance) {
-	app.post('/sessions', authenticate)
-	app.get('/me', {onRequest: [verifyJWT]}, profile)
+	app.post('/sessions', {schema: sessionSchema}, authenticate)
+	
 
-	app.addHook('onRequest', verifyJWT)
-
-	app.get('/users', listUsers)
-	app.post('/users', userRegistration)
-	app.get('/users/:id', showUser)
-	app.put('/users/:id', userUpdate)
-	app.delete('/users/:id', userDelete)
+	app.get('/me', {schema: profileSchema, 'onRequest': verifyJWT}, profile)
+	app.get('/users', {schema: getUsersSchema, 'onRequest': verifyJWT}, listUsers)
+	app.post('/users', {schema: createUserSchema}, userRegistration)
+	app.get('/users/:id', {schema: showUsersSchema, 'onRequest': verifyJWT}, showUser)
+	app.put('/users/:id', {schema: updateUserSchema, 'onRequest': verifyJWT}, userUpdate)
+	app.delete('/users/:id', {schema: deleteUsersSchema, 'onRequest': verifyJWT}, userDelete)
 
 }   
